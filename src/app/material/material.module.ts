64fdb14106afdcf5a900587b28f9f93d4b93103b
Jampool G <jampoolgarcia@gmail.com>
2023-05-19 07:06:59 -0400

    added styles

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,26 +2,55 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// core material
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
+// material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatCardModule} from '@angular/material/card';
 
-const material  = [
-  MatSlideToggleModule,
+// table material
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+
+// form material
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
+const MATERIAL = [
   MatToolbarModule,
-  MatTableModule
-];
-
+  MatIconModule,
+  MatButtonModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatCardModule
+]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    material
-  ],
+    MATERIAL
+  ], 
   exports: [
-    material
+    MATERIAL
   ]
 })
 export class MaterialModule { }
