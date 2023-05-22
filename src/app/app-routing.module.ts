e3e94fb2057cfa85320622c81163bd4712fac3fc
Jampool G <jampoolgarcia@gmail.com>
2023-05-22 16:31:02 -0400

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormEmployeeComponent } from './components/form-employee/form-employee.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: FormEmployeeComponent },
+  { path: '', component: ListEmployeeComponent },
+  { path: 'update/:id', component: FormEmployeeComponent },
+  { path: '**', redirectTo: '/' }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
